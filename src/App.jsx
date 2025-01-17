2e6dadaf31a2fd05bdbbebcd6507ff610c99973f
Nier <nierforprogramming@gmail.com>
2025-01-17 17:45:47 +0545

    Navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div class="container mx-auto px-4">
+    <Navbar />
+  </div>
+  
+  )
+}
+
+export default App
